ff7a1b5d01191882d5048ec5df71f5ee2a35eb5e
Ruben Magadan <138540824+Caguama94@users.noreply.github.com>
2025-10-27 18:07:31 -0600

    Renamed some folders
    Changed the name of tests folders to avoid namespaces issues.

--- SauceDemo.Tests/obj/Debug/net8.0/SauceDemo.Tests.AssemblyInfo.cs
+++ SauceDemo.Tests/obj/Debug/net8.0/SauceDemo.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SauceDemo.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b4b820883b0b6143d39b4c9907a3a42b0fd2426")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+731ef342912b35f9089047355460560429f4e052")]
 [assembly: System.Reflection.AssemblyProductAttribute("SauceDemo.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SauceDemo.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
